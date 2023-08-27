ce543e6fdebc3d74b4f45c146eabfaf4b671a8c2
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-08-27 18:08:54 +1000

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -2,9 +2,17 @@ clear;
 echo "LeafEnv Terminal - CA 2023";
 while true
 do
+  output_done=false;
   read -p "> " variable;
   if [[ $variable = "main" ]]; then
+    output_done=true;
     exit;
   fi;
-  command $variable;
+  if [[ $variable = "about" ]]; then
+    echo "LeafEnv Terminal is free software; created by LeafEnv-Contributed-Apps (LECA) to integrate LeafEnv mods. Integrating is easy using the default, LeafEnv package manager. Start adding more apps to LeafEnv today.";
+    output_done=true;
+  fi;
+  if [[ $output_done = false ]]; then
+    command $variable;
+  fi;
 done;
