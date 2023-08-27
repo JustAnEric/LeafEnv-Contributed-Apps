46470bb203cdb4c5e50e5b85c4893299d81d574c
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-08-27 17:12:32 +1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,21 +1,29 @@
-clear;
-echo "LeafEnv Contributed Apps";
-echo "#1) Play Snake";
-echo "#2) NANO Text Editor";
-echo "\n";
-echo "[Contribute]"
-
-read var;
-if [[ $var = "1" ]]; then
-  echo "Playing Snake...";
-  sleep 1;
-  bash ./LeafEnv-Contributed-Apps/snake.sh;
-fi;
-if [[ $var = "2" ]]; then
-  echo "Playing NANO Text Editor...";
-  sleep 1;
-  bash ./LeafEnv-Contributed-Apps/nano.sh;
-fi;
-if [[]]
-
-sleep 3;
+while true
+do
+  clear;
+  echo "LeafEnv Contributed Apps";
+  echo "#1) Play Snake";
+  echo "#2) NANO Text Editor";
+  echo "#3) Micro Text Editor"
+  echo "\n";
+  echo "[Contribute]"
+  
+  read var;
+  if [[ $var = "1" ]]; then
+    echo "Playing Snake...";
+    sleep 1;
+    bash ./LeafEnv-Contributed-Apps/snake.sh;
+  fi;
+  if [[ $var = "2" ]]; then
+    echo "Playing NANO Text Editor...";
+    sleep 1;
+    bash ./LeafEnv-Contributed-Apps/nano.sh;
+  fi;
+  if [[ $var = "3" ]]; then
+    echo "Playing Micro Text Editor...";
+    sleep 1;
+    bash ./LeafEnv-Contributed-Apps/micro.sh;
+  fi;
+  
+  sleep 3;
+done
